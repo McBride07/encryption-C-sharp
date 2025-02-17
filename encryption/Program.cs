d36e3ecf193b5b2c22a436e660f6fd67c9357f01
McBride07 <callummcbride@icloud.com>
2025-02-17 17:02:42 +0000

    finished padding

--- encryption/Program.cs
+++ encryption/Program.cs
@@ -3,7 +3,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Transactions;
 using System.Data.SqlTypes;
-
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 class Program {
 
     // Main Method
@@ -11,7 +12,7 @@ class Program {
     {
 
         Console.WriteLine("Main Method");
-        string input = "apple";
+        string input = "testing this thing";
         var encrypt = new MD4();
         encrypt.MD4func(input);
         
@@ -21,29 +22,71 @@ class Program {
 
 
 class MD4 {
-    public byte[] bytes = new byte[512];
+    
         public void MD4func(string input)
     {
-       byte[] bytes  = Encoding.ASCII.GetBytes(input);
-        foreach ( byte b in bytes)
+       List<byte> bytes = Encoding.ASCII.GetBytes(input).ToList();
+        int bitCount = bytes.Count * 8 ;
+        bytes.Add(128);
+        while (bytes.Count % 64 != 56)
         {
-            Console.Write(b);        }
-            int[] PADDING = [
-                        128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
-        int messagelength = input.Length;
-        int index = messagelength % 64;
-        if(index < 56)
+            bytes.Add(0);
+        }
+       int lengtharray = bytes.Count * 8;
+       string binary = Convert.ToString(lengtharray,2);
+       Console.WriteLine(binary + "<--------- this is the binary thing");
+        char[] temparray  = binary.ToCharArray();
+        
+        
+
+        for(int i = 8; i <  temparray.Length; i ++)
         {
-            int temp = 56 - index;
-            for(int i = bytes.Length; i < bytes.Length + temp; i++)
+            if(temparray[i] == '0')
             {
-                bytes[i] = Convert.ToByte(PADDING[i]);
-                Console.Write(bytes[i]);
+                bytes.Add(0);
+                
+            }
+            else
+            {
+                bytes.Add(1);
+                
+            }
+
+           
+           
+        }
+
+         for(int i = 0; i <  7; i ++)
+        {
+           
+           if(temparray[i] == '0')
+            {
+                bytes.Add(0);
+                
+            }
+            else
+            {
+                bytes.Add(1);
+                
             }
         }
+        
+       int total = 0;
+       for(int i = 0; i < bytes.Count; i ++)
+       {
+        
+        Console.Write(bytes[i]);
+        total =total +1;
+         }
+        
+
+
+
+       string word_A = "0x67452301";
+        string word_B = "0xefcdab89";
+       string  word_C = "0x98badcfe";
+        string word_D = "0x10325476";
+    }
                         
 
     }
@@ -52,8 +95,27 @@ class MD4 {
         
 
     
-}
+
       
         
 
 
+//byte[] bytes = Encoding.ASCII.GetBytes(input);
+        //foreach ( byte b in bytes)
+        //{
+           // Console.Write(b);        }
+           // int[] PADDING = [
+                     //   128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     //   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                     //   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      //  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+      //  int messagelength = input.Length;
+       // int index = messagelength % 64;
+       // if(index < 56)
+       // {
+          //  int temp = 56 - index;
+           // for(int i = bytes.Length; i < bytes.Length + temp; i++)
+           // {
+           //     bytes[i] = Convert.ToByte(PADDING[i]);
+           //     Console.Write(bytes[i]);
+           // }
